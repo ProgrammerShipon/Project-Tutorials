a96d96aee2bac75626313c4c03d47d331103b2bd
Shipon Hossen Raju <msshipon234@gmail.com>
2022-10-01 22:00:41 +0600

    file stacture and tailwind.css installation some problem and no-test

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/tailwind.css/input/**/*.{html,js}"],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
